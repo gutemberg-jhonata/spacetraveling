07bbfb27b76d068c80c316c677d36e656a4371e3
Jhonata <gutemberg.jhonata@gmail.com>
2021-06-01 08:53:23 -0300

    Styling home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { GetStaticProps } from 'next';
-import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -26,7 +28,40 @@ interface HomeProps {
 }
 
 export default function Home() {
-  return <Header></Header>;
+  return (
+    <div className={styles.homeContainer}>
+      <Header />
+      <div className={styles.posts}>
+        <a href="/">
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <span>
+              <FiCalendar /> 15 Mar 2021
+            </span>
+            <span>
+              <FiUser /> Joseph Oliveira
+            </span>
+          </div>
+        </a>
+        <a href="/">
+          <h1>Criando um app CRA do zero</h1>
+          <p>
+            Tudo sobre como criar a sua primeira aplicação utilizando Create
+            React App
+          </p>
+          <div className={styles.info}>
+            <span>
+              <FiCalendar /> 19 Abr 2021
+            </span>
+            <span>
+              <FiUser /> Danilo Vieira
+            </span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 // export const getStaticProps = async () => {
